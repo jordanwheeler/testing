794e582a685661f7cc3830ad46b2b0d7596274f0
Jordan Wheeler <jordan.wheeler@jadedpixel.com>
2013-07-18 12:16:17 -0400

    checking this

--- lib/active_merchant/billing/gateways/paypal/paypal_common_api.rb
+++ lib/active_merchant/billing/gateways/paypal/paypal_common_api.rb
@@ -538,7 +538,9 @@ module ActiveMerchant #:nodoc:
       end
 
       def add_payment_details_items_xml(xml, options, currency_code)
-        non_decimal_discount = true if NON_DECIMAL_CURRENCIES.include?(currency_code.to_s) && options[:items][-1][:amount].to_i < 0
+        non_decimal_discount = true if NON_DECIMAL_CURRENCIES.include?(currency_code) && options[:items][-1][:amount].to_i < 0
+        puts "YA BUDDY, DISCOUNT IS #{non_decimal_discount}"
+        puts "currency code is a #{currency_code.class}"
         item_total = 0 if non_decimal_discount
         options[:items].each do |item|
           xml.tag! 'n2:PaymentDetailsItem' do
