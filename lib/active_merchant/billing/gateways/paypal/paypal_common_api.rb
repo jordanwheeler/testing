4884dc65c2c5d7521af80e4103587db1d7b540be
Jordan Wheeler <jordan.wheeler@jadedpixel.com>
2013-07-17 23:39:11 -0400

    alright this makes even more sense

--- lib/active_merchant/billing/gateways/paypal/paypal_common_api.rb
+++ lib/active_merchant/billing/gateways/paypal/paypal_common_api.rb
@@ -548,7 +548,7 @@ module ActiveMerchant #:nodoc:
             if item[:amount]
               localized = localized_amount(item[:amount], currency_code)
               total += localized.to_i if non_decimal
-              if (item == options[:items].last && item[:name] == 'Discount' && non_decimal)
+              if (item[:amount] < 0 && non_decimal)
                 diff = (localized_amount(options[:subtotal], currency_code)).to_i - total
                 localized = localized.to_i + diff
               end
